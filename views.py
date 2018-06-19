348d30afc0b6985c2c3339264f44147e670445b4
Marta <marta.rose@gmail.com>
2018-06-18 17:08:23 -0700

    finished assignment

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 '''
@@ -13,5 +13,24 @@ def index(request):
 '''
 
 def index(request):
-    #response = "placeholder"
+    response = "placeholder to later display the list of blogs"
+    return HttpResponse(response)
+
+def new(request):
+    response = "placeholder to display a new form to create a new blog"
+    return HttpResponse(response)
+
+def create(request):
+    return redirect(index)
+
+def show(request, number):
+    response = "placeholder to display blog " + str(number)
+    return HttpResponse(response)
+
+def edit(request, number):
+    response = "placeholder to edit blog " + str(number)
+    return HttpResponse(response)
+
+def delete(request, number):
+    response = "placeholder to delete blog " + str(number)
     return HttpResponse(response)
